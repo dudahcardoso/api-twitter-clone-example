a7b74654e0a554627d98fde91cda20cbb02bdc16
dudahcardoso <mcardoso@blueedtech.com.br>
2022-05-04 14:18:23 -0300

    connect database

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDatabase = require("./database/database");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase();
 app.use(cors());
 app.use(express.json());
 
