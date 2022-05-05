2842b2a5390b82bbaa962684c5426a2b369c5b2c
dudahcardoso <mcardoso@blueedtech.com.br>
2022-05-05 18:22:49 -0300

    login controller and service

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,7 +1,24 @@
 const authService = require('./auth.service');
 
+const bcrypt = require("bcryptjs");
+
 const loginController = async(req, res) => {
-    res.send({message:"Login Ok"});
+    const {email, password } = req.body;
+
+    const user = await authService.loginService(email);
+
+    if(!user){
+        return res.status(400).send({message: "Usuário não encontrado! "})
+    };
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(400).send({message: "Senha inválida!"})
+
+    }
+
+    res.send(user);
 };
 
 module.exports = { loginController};
---
+++ src/auth/auth.service.js
@@ -0,0 +1,7 @@
+
+const User = require('../users/User');
+
+const loginService = (email) => User.findOne({email: email}).select("+password");
+
+
+module.exports = { loginService}
