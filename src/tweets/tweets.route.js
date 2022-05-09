2a1d8c5c6f4c0e3cb0163c8f0ce6e1f5c63955fd
dudahcardoso <mcardoso@blueedtech.com.br>
2022-05-09 15:00:18 -0300

    add search tweets

--- src/tweets/tweets.route.js
+++ src/tweets/tweets.route.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 const tweetController = require("./tweets.controller");
 const authMiddleware = require("../auth/auth.middleware");
 
-router.post("/", authMiddleware, tweetController.createTweetController);
+router.post("/create", authMiddleware, tweetController.createTweetController);
 router.get("/", authMiddleware, tweetController.findAllTweetsController);
+router.get("/search", authMiddleware, tweetController.searchTweetController);
 
 module.exports = router;
--- src/tweets/tweets.service.js
+++ src/tweets/tweets.service.js
@@ -6,4 +6,15 @@ const createTweetService = (message, userId) =>
 const findAllTweetsService = () =>
   Tweet.find().sort({ _id: -1 }).populate("user");
 
-module.exports = { createTweetService, findAllTweetsService };
+const searchTweetService = (message) =>
+  Tweet.find({
+    message: { $regex: `${message || ""}`, $options: "i" },
+  })
+    .sort({ _id: -1 })
+    .populate("user");
+
+module.exports = {
+  createTweetService,
+  findAllTweetsService,
+  searchTweetService,
+};
