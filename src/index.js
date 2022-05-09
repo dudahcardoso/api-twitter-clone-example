3b9a0eeb744d63a2f84abf818a0429598b6faa25
dudahcardoso <mcardoso@blueedtech.com.br>
2022-05-09 13:28:52 -0300

    construção do tweet

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const connectDatabase = require("./database/database");
 const userRoute = require("./users/users.route");
 const authRoute = require("./auth/auth.route");
-
+const tweetsRoute = require("./tweets/tweets.route")
 const port = process.env.PORT || 3001;
 const app = express();
 
@@ -18,6 +18,7 @@ app.use(express.json());
 
 app.use("/users", userRoute);
 app.use("/auth", authRoute);
+app.use("/tweets", tweetsRoute);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
